5b72176cfdd3e48a79c4ff63cbc3aff70d1b4c49
edisgipu <eagiraldo@sura.com.co>
2023-03-01 18:57:11 -0500

    Creacion de usuario bd

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,45 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario')
 
-const crearUsuario = (req, res = response) =>{
+const crearUsuario = async(req, res = response) =>{
     const{ email, name, password }= req.body;
+
+    try {
+    //verificacion de email
+        const usuario = await Usuario.findOne ({ email })
+        if (usuario) {
+            return res.status(200).json({
+                ok: true,
+                msg: 'Correo ya registrado'
+            });
+        }
+
+    //crear usuario con el modelo
+        const bdusuario= new Usuario( req.body);
+
+    //encriptar contraseña
+
+    //generar JWT
+
+    //crear usuario en bd
+        await bdusuario.save();
+    
+    //respuesta servicio exitosa
     return res.status(200).json({
         ok: true,
-        msg: 'Crar usuario'
+        uid: bdusuario.id,
+        name: name
     });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: true,
+            msg: 'Hablar con el administrador'
+        });
+    }
+
+
 }
 
 const loguearUsuario = (req, res = response) =>{
